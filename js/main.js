e918792fca8ce13486ed60b2eb88fef4ff036d78
t1mch1k12 <kim228455@gmail.com>
2026-03-25 17:57:39 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
+<canvas id="gameCanvas" width="800" height="600" style="border:1px solid black;"></canvas>
 
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
+
+function gameLoop() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+    // Здесь будут вызовы
+    
+    requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
